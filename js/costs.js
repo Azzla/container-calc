4b458e01fc4ee4901604e721e1a2e384f978fab2
Carson <azzlamusic@hotmail.com>
2020-11-08 15:01:42 -0700

    added costs module

---
+++ js/costs.js
@@ -0,0 +1,38 @@
+export const containerCosts =
+[
+	{zip: "37128", depot: "First Coast Depot", prices: {
+		stan_20: 1000,
+		stan_40: 2000,
+		hc_40: 2500
+	}},
+	{zip: "38654", depot: "ConGlobal Industries", prices: {
+		stan_20: 1020,
+		stan_40: 2000,
+		hc_40: 2500
+	}},
+	{zip: "84032", depot: "Picorp Inc", prices: {
+		stan_20: 1200,
+		stan_40: 2000,
+		hc_40: 2500
+	}},
+	{zip: "51106", depot: "InTransit Container", prices: {
+		stan_20: 1400,
+		stan_40: 2000,
+		hc_40: 2500
+	}},
+	{zip: "12203", depot: "Davidson Transportation", prices: {
+		stan_20: 1050,
+		stan_40: 2000,
+		hc_40: 2500
+	}},
+	{zip: "44406", depot: "Winners Container Serv", prices: {
+		stan_20: 1700,
+		stan_40: 2000,
+		hc_40: 2500
+	}},
+	{zip: "20782", depot: "Arrowhead Depot", prices: {
+		stan_20: 1300,
+		stan_40: 2000,
+		hc_40: 2500
+	}}
+];
